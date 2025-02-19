67ddf41935e8ef4c70c3574e1b73f5b58100829c
Md Siyam <mdsiyam9424@gmail.com>
2025-02-19 16:45:13 +0600

    make responsive website

--- src/pages/Home/KeepUs.jsx
+++ src/pages/Home/KeepUs.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import KeepusImage from "../../assets/about-collage.jpg"
 import { motion } from "motion/react"
 const KeepUs = () => {
@@ -12,10 +13,13 @@ const KeepUs = () => {
             <div className="flex-1 sm:mt-6">
                 <h1 className="text-5xl font-serif ">Go Exotic Palces!</h1>
                 <p className="my-4 opacity-70">Planning your next adventure? Let us be your guide! Whether you're dreaming of serene beaches, bustling cities, or breathtaking mountains, we are here to help you explore the destinations that excite you the most. Save your favorite locations, track your travel plans, and receive personalized recommendations to make every trip unforgettable.</p>
+                <Link to={"https://www.booking.com"}>
                 <motion.button 
                  whileHover={{ scale: 1.05 }}
                  whileTap={{ scale: 0.8 }}
                  className="px-10 py-3 rounded-full border-2 border-[#1a73e8] text-[#1a73e8] font-semibold hover:bg-[#1a73e8] hover:text-white">Book Now !</motion.button>
+                </Link>
+                
             </div>           
         </div>
         </div>
