310f3a755c961206be891185f52a30823ed350dd
Md Siyam <mdsiyam9424@gmail.com>
2025-02-19 16:38:27 +0600

    make button clickable

--- src/pages/Home/DareToExplore.jsx
+++ src/pages/Home/DareToExplore.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import Image from '../../assets/man-covering-his-face-with-ukulele-guitar.jpg'
 
 const DareToExplore = () => {
@@ -6,7 +7,10 @@ const DareToExplore = () => {
             <div className='flex-1 pl-16 lg:h-[473px] h-[350px] flex flex-col justify-center bg-slate-700 text-white'>
                 <h1 className="text-5xl font-serif ">Dare To Explore</h1>
                 <p className='opacity-70 my-3 w-[90%]'>The world is vast, waiting to be discovered. Beyond the familiar streets and well-known landmarks lie hidden wonders, thrilling adventures, and cultures rich with history. Are you ready to step out of your comfort zone and embrace the unknown?</p>
+                <Link to="https://www.dominiquedebaydmc.com/dare-to-explore-south-africa">
                 <button  className="px-10 py-3 w-44 rounded-full border-2 border-[#1a73e8] text-[#1a73e8] font-semibold hover:bg-[#1a73e8] hover:text-white">See More</button>
+                </Link>
+            
             </div>
             <div className='flex-1'>
                 <img className='lg:h-[473px]' src={Image} alt="" />
--- src/pages/Home/JoinCommunity.jsx
+++ src/pages/Home/JoinCommunity.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import JoinCoummunityImg from "../../assets/join-our-community.jpg"
 import { motion } from "motion/react"
 const JoinCommunity = () => {
@@ -6,10 +7,12 @@ const JoinCommunity = () => {
             <div className=" text-center lg:text-start">
                 <h1 className="text-3xl md:text-4xl lg:text-5xl font-serif">Want To Know More About Us ?</h1>
                 <p className="my-4 opacity-70">Be a part of a vibrant travel community where adventure meets connection! Whether you're a seasoned traveler or just starting your journey, our community is the perfect place to share experiences, discover hidden gems, and connect with like-minded explorers from around the world.</p>
+                <Link to="https://www.linkedin.com/in/md-siyam-d942404/">
                 <motion.button
                     whileHover={{ scale: 1.05 }}
                     whileTap={{ scale: 0.99 }}
-                    className="px-10 py-3 rounded-full border-2 border-[#1a73e8] text-[#1a73e8] font-semibold hover:bg-[#1a73e8] hover:text-white">Join Our Community</motion.button>
+                    className="px-10 py-3 rounded-full border-2 border-[#1a73e8] text-[#1a73e8] font-semibold hover:bg-[#1a73e8] hover:text-white">Join Our Community</motion.button></Link>
+               
             </div>
             <div>
                 <img className="w-96 lg:w-full mx-auto" src={JoinCoummunityImg} alt="" />
